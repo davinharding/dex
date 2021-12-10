8fca4abe20eda315f0350f726dcf20341a9a1d3b
Davin <davinlharding@gmail.com>
2021-12-10 13:02:12 -0500

    Smart contracts+ migrations completed, front end in progress

---
+++ client/src/Wallet.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const DIRECTION = {
+    WITHDRAW: 'WITHDRAW',
+    DEPOSIT: 'DEPOSIT'
+};
+
+function Wallet() {
+    const [direction, setDirection] = 
+}
